b067ca659608b88713eb8d6b3d53f6ce7fd3a4b5
jahid-khan <jahidkhantuhin@gmail.com>
2026-02-28 00:24:44 +0600

    make the cart count dynamically

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-let productsHTML = "";
+let productsHTML = '';
+
 products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
@@ -50,7 +51,40 @@ products.forEach((product) => {
         Add to Cart
       </button>
     </div>
-    `;
+  `;
 });
 
-document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+    });
+  });
